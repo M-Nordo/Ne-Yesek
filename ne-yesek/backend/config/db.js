4122597deab84cc9e3b6952f79d328c92444c8ba
Mehmet Yayla <69403373+M-Nordo@users.noreply.github.com>
2024-08-18 17:07:58 +0300

    Update db.js

--- ne-yesek/backend/config/db.js
+++ ne-yesek/backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://nordo:nordo%4055@cluster0.ftxeelc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Bağlandı"));
+    await mongoose.connect('mongodb_url').then(()=>console.log("DB Bağlandı"));
 }
 
 //mongodb+srv://nordo:nordo@55@cluster0.ftxeelc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
